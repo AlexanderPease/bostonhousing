69d4906c4ae1656ec4e6b510d87a850cb80a65aa
AlexanderPease <zander@nomadhealth.com>
2018-05-18 12:59:07 -0400

    Cleanup

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# From https://medium.com/@haydar_ai/learning-data-science-day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef
+# From https://medium.com/@haydar_ai/learning-data-science-
+# day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef
 
-from matplotlib import rcParams # special matplotlib argument for improved plots
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas
 import seaborn as sns
-import scipy.stats as stats
-# import sklearn
 from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
 
 sns.set_style("whitegrid")
 sns.set_context("poster")
@@ -35,8 +31,8 @@ class BostonData(object):
         X_train, X_test, Y_train, Y_test = train_test_split(
             self.features,
             self.target,
-            test_size = 0.33,
-            random_state = 5
+            test_size=0.33,
+            random_state=5
         )
 
         # Train the model
@@ -46,16 +42,18 @@ class BostonData(object):
         # These are predicted target values
         Y_pred = lm.predict(X_test)
 
-        # Visualize predicted vs actual Y test values...
-        # Couldn't get visualizations to run
-        # plt.scatter(Y_test, Y_pred)
-        # plt.xlabel("Prices: $Y_i$")
-        # plt.ylabel("Predicted prices: $\hat{Y}_i$")
-        # plt.title("Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$")
-
-        # ...which is mathematically capture in Mean Squared Error
+        # Fit of model is mathematically capture in Mean Squared Error
         mse = mean_squared_error(Y_test, Y_pred)
         print('Mean Squared Error: {}'.format(mse))
+
+
+def visualize_results(target_test, target_pred):
+    # Visualize predicted vs actual Y test values
+    # Couldn't get visualizations to run
+    plt.scatter(target_test, target_pred)
+    plt.xlabel("Prices: $Y_i$")
+    plt.ylabel("Predicted prices: $\hat{Y}_i$")
+    plt.title("Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$")
 
 
 if __name__ == "__main__":
